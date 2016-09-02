2e8e8ea253ecb8a2e55ab860691c31d190e939a8
Mai X <mxio@users.noreply.github.com>
2016-09-02 12:12:00 -0600

    Add files via upload

---
+++ Forms/Signup/signup.js
@@ -0,0 +1,18 @@
+var signupForm = document.getElementById('signup');
+var email = document.getElementById('email');
+
+signupForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        if (!validator.isEmailAddress(email.value)) {
+             signupForm.className = "invalid";
+             email.className = "invalid"; 
+        }
+
+}, false);
+
+email.addEventListener('input', function(event) {
+    if (validator.isEmailAddress(email.value)) {
+        signupForm.className = "valid";
+        email.className = "valid";
+    }
+}, false);
