7cb0e5a5a6fa2469885d3a0c877b16c34d84f592
mxio <mxio@users.noreply.github.com>
2016-09-05 14:00:47 -0600

    added first name validation

--- Forms/Signup/signup.js
+++ Forms/Signup/signup.js
@@ -1,18 +1,29 @@
 var signupForm = document.getElementById('signup');
 var email = document.getElementById('email');
+var firstname = document.getElementById('firstname')
 
 signupForm.addEventListener('submit', function (event) {
         event.preventDefault();
-        if (!validator.isEmailAddress(email.value)) {
-             signupForm.className = "invalid";
-             email.className = "invalid"; 
+        if (validator.isEmpty(firstname.value)) {
+        	firstname.className = "fname invalid";
         }
 
+        else if (!validator.isEmailAddress(email.value)) {
+             email.className = "email invalid"; 
+        }
+}, false);
+
+firstname.addEventListener('input', function(event) {
+	if (!validator.isEmpty(firstname.value)) {
+		signupForm.className = "form-1 valid";
+		firstname.className = "fname valid";
+	}
 }, false);
 
 email.addEventListener('input', function(event) {
     if (validator.isEmailAddress(email.value)) {
-        signupForm.className = "valid";
-        email.className = "valid";
+        signupForm.className = "form-1 valid";
+        email.className = "email valid";
     }
 }, false);
+
