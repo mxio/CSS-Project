560ee8af2f4971a1267b57e08b61ced0bb16034e
mxio <mxio@users.noreply.github.com>
2016-09-05 15:49:21 -0600

    added camelCase to firstname

--- Forms/Signup/signup.js
+++ Forms/Signup/signup.js
@@ -1,22 +1,23 @@
 var signupForm = document.getElementById('signup');
 var email = document.getElementById('email');
-var firstname = document.getElementById('firstname')
+var firstName = document.getElementById('firstName');
+var lastName
 
 signupForm.addEventListener('submit', function (event) {
         event.preventDefault();
-        if (validator.isEmpty(firstname.value) || validator.isLength(firstname.value, 2)) {
+        if (validator.isEmpty(firstName.value) || validator.isLength(firstName.value, 2)) {
         	signupForm.className = "form-1 invalid";
-        	firstname.className = "fname invalid";
+        	firstName.className = "fname invalid";
         }
         if (validator.isEmpty(email.value) || !validator.isEmailAddress(email.value)) {
              email.className = "email invalid"; 
         }
 }, false);
 
-firstname.addEventListener('input', function(event) {
-	if (!validator.isEmpty(firstname.value) || validator.isOfLength(firstname.value, 3)) {
+firstName.addEventListener('input', function(event) {
+	if (!validator.isEmpty(firstName.value) || validator.isOfLength(firstName.value, 3)) {
 		signupForm.className = "form-1 valid";
-		firstname.className = "fname valid";
+		firstName.className = "fname valid";
 	}
 }, false);
 
