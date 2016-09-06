eedb836887469a435369d8ddd4d6997b7c2eb07d
mxio <mxio@users.noreply.github.com>
2016-09-06 11:54:06 -0600

    added last name validation

--- Forms/Signup/signup.js
+++ Forms/Signup/signup.js
@@ -1,12 +1,17 @@
 var signupForm = document.getElementById('signup');
 var email = document.getElementById('email');
 var firstName = document.getElementById('firstname');
+var lastName = document.getElementById('lastname');
 
 signupForm.addEventListener('submit', function (event) {
         event.preventDefault();
         if (validator.isEmpty(firstName.value) || validator.isLength(firstName.value, 2)) {
         	signupForm.className = "form-1 invalid";
         	firstName.className = "fname invalid";
+        }
+        if (validator.isEmpty(lastName.value) || validator.isLength(lastName.value, 2)) {
+        	signupForm.className = "form-1 invalid";
+        	lastName.className = "lname invalid";
         }
         if (validator.isEmpty(email.value) || !validator.isEmailAddress(email.value)) {
              email.className = "email invalid"; 
@@ -17,6 +22,13 @@ firstName.addEventListener('input', function(event) {
 	if (!validator.isEmpty(firstName.value) || validator.isOfLength(firstName.value, 3)) {
 		signupForm.className = "form-1 valid";
 		firstName.className = "fname valid";
+	}
+}, false);
+
+lastName.addEventListener('input', function(event) {
+	if (!validator.isEmpty(lastName.value) || validator.isOfLength(lastName.value, 3)) {
+		signupForm.className = "form-1 valid";
+		lastName.className = "lname valid";
 	}
 }, false);
 
