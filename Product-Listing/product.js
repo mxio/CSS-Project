7fe0d00eccc4ef4d6d6b1eb08992b872334b526c
mxio <mxio@users.noreply.github.com>
2016-09-21 13:28:42 -0600

    Removed button styling for "Keep Shopping" Fixed toggling problem where two clicks are required initially

--- Product-Listing/product.js
+++ Product-Listing/product.js
@@ -3,17 +3,15 @@ var shoppingCart = document.getElementById('shopping-cart');
 
 showCart.addEventListener('click', function(event) {
 	event.preventDefault();
-	
+
 	if (showCart.innerHTML === "Show Cart") {
-		showCart.onclick = function () {
-			shoppingCart.className = "show";
-			showCart.innerHTML = "Hide Cart";
-		}
+		shoppingCart.className = "show";
+		showCart.innerHTML = "Hide Cart";
+		
 	}
 	else if (showCart.innerHTML === "Hide Cart") {
-		showCart.onclick = function () {
-			shoppingCart.className = "hide";
-			showCart.innerHTML = "Show Cart";
-		}
+		shoppingCart.className = "hide";
+		showCart.innerHTML = "Show Cart";
+		
 	}
 }, false);
