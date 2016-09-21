e074568f89812ff36244ef7cf93c7a423033ba53
mxio <mxio@users.noreply.github.com>
2016-09-21 12:37:19 -0600

    Changed position of shopping cart when toggled

--- Product-Listing/product.js
+++ Product-Listing/product.js
@@ -2,5 +2,10 @@ var showCart = document.getElementById('show-cart');
 var shoppingCart = document.getElementById('shopping-cart');
 
 showCart.addEventListener('click', function(event) {
+	event.preventDefault();
 	shoppingCart.className = "show";
+}, false);
+
+shoppingCart.addEventListener('click', function(event) {
+	shoppingCart.className = "hide";
 }, false);
