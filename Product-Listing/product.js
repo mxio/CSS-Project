420f77672a0de4031184f1b7f2f8177d32855d2b
mxio <mxio@users.noreply.github.com>
2016-09-22 09:18:32 -0600

    Wrote pseudo code for product listing object

--- Product-Listing/product.js
+++ Product-Listing/product.js
@@ -1,5 +1,6 @@
 var showCart = document.getElementById('show-cart');
 var shoppingCart = document.getElementById('shopping-cart');
+var addToCart = document.getElementsByClassName('add-to-cart');
 
 showCart.addEventListener('click', function(event) {
 	event.preventDefault();
@@ -7,7 +8,6 @@ showCart.addEventListener('click', function(event) {
 	if (showCart.innerHTML === "Show Cart") {
 		shoppingCart.className = "show";
 		showCart.innerHTML = "Hide Cart";
-		
 	}
 	else if (showCart.innerHTML === "Hide Cart") {
 		shoppingCart.className = "hide";
@@ -15,3 +15,85 @@ showCart.addEventListener('click', function(event) {
 		
 	}
 }, false);
+
+addToCart.addEventListener('click', function(event) {
+	//get current input's value
+}, false); 
+
+var cart = {
+	"items": [
+		{
+			"id": "01",
+			"name": "Tee",
+			"price": 14.99,
+			"count": 1
+		},
+		{
+			"id": "02",
+			"name": "Tank",
+			"price": 10.99,
+			"count": 1
+		}
+		{
+			"id": "03",
+			"name": "Sweater",
+			"price": 19.99,
+			"count": 1
+		}
+		{
+			"id": "04",
+			"name": "Skirt",
+			"price": 16.99,
+			"count": 1
+		}
+		{
+			"id": "05",
+			"name": "Jean",
+			"price": 24.99,
+			"count": 1
+		}
+		{
+			"id": "06",
+			"name": "Pant",
+			"price": 27.99,
+			"count": 1
+		}
+		{
+			"id": "07",
+			"name": "Dress",
+			"price": 45.00,
+			"count": 1
+		}
+		{
+			"id": "08",
+			"name": "Shoe",
+			"price": 15.99,
+			"count": 1
+		}
+		{
+			"id": "Scarf",
+			"name": "Loose T-shirt",
+			"price": 8.99,
+			"count": 1
+		}
+		{
+			"id": "010",
+			"name": "Blouse",
+			"price": 29.99,
+			"count": 1
+		}
+		{
+			"id": "11",
+			"name": "Purse",
+			"price": 34.99,
+			"count": 1
+		}
+		{
+			"id": "12",
+			"name": "Sunglass",
+			"price": 8.99,
+			"count": 1
+		}
+	],
+	"count": 0
+};
