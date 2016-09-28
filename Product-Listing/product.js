8f7c748d5f34ea4680c8db5f2960d0007a1acc82
mxio <mxio@users.noreply.github.com>
2016-09-28 13:16:57 -0600

    Redid shopping cart by breaking down into separate functions

--- Product-Listing/product.js
+++ Product-Listing/product.js
@@ -1,12 +1,11 @@
 var showCart = document.getElementById('show-cart');
 var shoppingCart = document.getElementById('shopping-cart');
-var addToCartIcons = document.querySelectorAll('.material-icons');
 var cartOnPage = document.getElementById('cart-on-page');
-var shoppingCartTotalQty = document.getElementById('shopping-cart-total');
-var totalPrice = document.getElementById('shopping-cart-total-price');
-var itemTotal = document.getElementById('item-total');
 var quantity = document.getElementById('total-qty');
-
+var cartOnPage = document.getElementById('cart-on-page');
+var itemTotal = document.getElementById('item-total');
+var shoppingCartTotalQty = document.getElementById('shopping-cart-total');
+var addToCartIcons = document.querySelectorAll('.material-icons');
 
 showCart.addEventListener('click', function(event) {
 	event.preventDefault();
@@ -19,164 +18,70 @@ showCart.addEventListener('click', function(event) {
 		shoppingCart.className = "hide";
 		showCart.innerHTML = "Show Cart";
 	}
-
 }, false);
 
 
-for (var i = 0; i < addToCartIcons.length; i++) {
-	addToCartIcons[i].addEventListener('click', function(event) {
-		event.preventDefault();
-		addToCart(event);
-	}, false); 
-}
+//add or update
+function add(id) {
 
-function addToCart(event) {
-	var product = event.target.parentNode.parentNode.parentNode.parentNode;
-	var productID = product.id;
-	var productQty = product.querySelector('.quantity').value;
+	var product = document.getElementById(id);
+    var productID = product.id;
+    var productQty = parseInt(product.querySelector('.quantity').value);
+    var productName = product.querySelector('.name').innerHTML;
+    var productPrice = product.querySelector('.price').innerHTML;
+    	productPrice = parseFloat(productPrice.substr(1, 5)).toFixed(2);
+    var index = findIndex();
 
-	var qtyValueToNum = parseInt(productQty);
-	cart.count += qtyValueToNum;
-	cartOnPage.innerHTML = cart.count;
+	item = {
+        "id": productID,
+        "name": productName,
+        "price": productPrice,
+        "quantity": productQty,
+        "totalPrice": productPrice * productQty
+    }
 
-
-	itemTotal.innerHTML += '<section class="cart-row"><div class="shopping-cart-img"><img src="#"></div>' + '<div class="col-1">' + cart.items[productID].name + "</div>" + " " + '<div class="col-2"><form><input class="box cart-qty-box" type="text" value="' + productQty + '"></input></form><button class="change" href="#">Change Quantity</button></div>' + '<div class="col-3">$' + (productQty * cart.items[productID].price) + "</div></section>";
-	
-	
-	//Price Total
-	priceTotal.push(productQty * cart.items[productID].price);
-
-	var total = 0;
-
-	for (var e = 0; e < priceTotal.length; e++) {
-    	total += priceTotal[e];
-	}
-	
-	shoppingCartTotalQty.innerHTML = "$" + parseFloat(total).toFixed(2);
-
-	//Qty Total
-	qtyTotal.push(qtyValueToNum);
-	var totalQty = 0;
-
-	for (var j = 0; j < qtyTotal.length; j++) {
-		totalQty += qtyTotal[j];
-	}
-
-	quantity.innerHTML = totalQty;
-
-	//show cart if quantity >= 1
-	if (totalQty >= 1) {
+    //if item's id does not exist, push to cart, else change qty
+    if (index === -1) {	
+	    cart.items.push(item);
+    	cart.count += item.quantity;
+    }
+    else cart.items[index].quantity += item.quantity; 
+    	cart.items[index].totalPrice = cart.items[index].price * cart.items[index].quantity; 
+    
+    if (cart.count > 0) {
 		shoppingCart.className = "show";
 		showCart.innerHTML = "Hide Cart";
 	}
-
-	var changeQty = document.querySelectorAll('.change');
-
-	//change quantity text boxes
-	for (var a = 0; a < changeQty.length; a++) {
-		changeQty[a].addEventListener('click', function(event) {
-			event.preventDefault();
-			changeQuantity(event);
-		}, false); 
-	}
-
-	function changeQuantity(event) {
-		var qtyBoxParent = event.target.parentNode.parentNode;
-		var qtyBox = qtyBoxParent.querySelector('.cart-qty-box').value;
-		
-		var cartRowParent = qtyBoxParent.parentNode;
-		var child = cartRowParent.querySelector('section');
-		
-		var qtyValueToNum = parseInt(qtyBox);
-		cart.count = 0;
-		cart.count += qtyValueToNum;
-		cartOnPage.innerHTML = cart.count;
-
-		if (qtyValueToNum === 0) {
-			qtyBoxParent.remove(child.lastChild);
-		}
-	}
 }
 
-var priceTotal = [];
-var qtyTotal = [];
+//find index of item in cart items 
+function findIndex() {
+	var indexOf = -1;
 
+	for (var i = 0; i < cart.items.length; i++) {
+		if (item.id === cart.items[i].id) {
+			indexOf = cart.items.indexOf(cart.items[i]);
+			alert(indexOf);
+		}
+	}
+	return indexOf;
+}
 
+//populate shopping cart
+
+//change quantity in shopping cart
+
+//totals
+	//loop for quantity total
+	//loop for price total
+
+//add event listener to each icon
 
 var cart = {
 	"items": [
-		{
-			"id": "01",
-			"name": "Tee",
-			"price": 14.99,
-			"count": 1
-		},
-		{
-			"id": "02",
-			"name": "Tank",
-			"price": 10.99,
-			"count": 1
-		},
-		{
-			"id": "03",
-			"name": "Sweater",
-			"price": 19.99,
-			"count": 1
-		},
-		{
-			"id": "04",
-			"name": "Skirt",
-			"price": 16.99,
-			"count": 1
-		},
-		{
-			"id": "05",
-			"name": "Jean",
-			"price": 24.99,
-			"count": 1
-		},
-		{
-			"id": "06",
-			"name": "Pant",
-			"price": 27.99,
-			"count": 1
-		},
-		{
-			"id": "07",
-			"name": "Dress",
-			"price": 45.00,
-			"count": 1
-		},
-		{
-			"id": "08",
-			"name": "Shoe",
-			"price": 15.99,
-			"count": 1
-		},
-		{
-			"id": "Scarf",
-			"name": "Loose T-shirt",
-			"price": 8.99,
-			"count": 1
-		},
-		{
-			"id": "010",
-			"name": "Blouse",
-			"price": 29.99,
-			"count": 1
-		},
-		{
-			"id": "11",
-			"name": "Purse",
-			"price": 34.99,
-			"count": 1
-		},
-		{
-			"id": "12",
-			"name": "Sunglass",
-			"price": 8.99,
-			"count": 1
-		}
+		
 	],
-	"count": 0
+	"count": 0,
+	"total": 0
 };
+
