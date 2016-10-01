8e0b02e80d944f592a907e2a80b2462e94b55632
mxio <mxio@users.noreply.github.com>
2016-10-01 11:57:51 -0600

    Fixed col-3 total so it updates when quantity is changed

--- Product-Listing/product.js
+++ Product-Listing/product.js
@@ -23,7 +23,7 @@ showCart.addEventListener('click', function(event) {
 
 
 
-//add or update
+//add items to cart
 function add(id) {
 
 	var product = document.getElementById(id);
@@ -46,7 +46,7 @@ function add(id) {
     if (index === -1) {	
 	    cart.items.push(item);
     }
-    else {
+    else { //if item exists, remove it because it adds another one, then update quantity
     	document.getElementById("section"+cart.items[index].id).remove();
     	cart.items[index].quantity += productQty;
     	cart.items[index].totalPrice = cart.items[index].price * cart.items[index].quantity;
@@ -69,8 +69,9 @@ function findIndex(id) {
 	return indexOf;
 }
 
+//show shopping cart section if there is one or more items in the cart
 function showCartOne() {
-    if (cart.count >= 1) {
+    if (cart.count > 0) {
 		shoppingCart.className = "show";
 		showCart.innerHTML = "Hide Cart";
 	}
@@ -81,9 +82,12 @@ function populateShoppingCart(index, id) {
 		//index is undefined so need to add the variable here again while passing index and id as parameters
 		var index = findIndex(id);
 
-		itemTotal.innerHTML += '<section id="section'+cart.items[index].id+'" class="cart-row"><div class="shopping-cart-img"><img src="#"></div>' + '<div class="col-1">' + cart.items[index].name + "</div>" + " " + '<div class="col-2"><form><input class="box cart-qty-box" value="' + cart.items[index].quantity + '"></input></form><button class="change" itemid="'+cart.items[index].id+'" href="#">Change Quantity</button></div>' + '<div class="col-3">$' + cart.items[index].totalPrice + "</div></section>";
+		itemTotal.innerHTML += '<section id="section'+cart.items[index].id+'" class="cart-row"><div class="shopping-cart-img"><img src="#"></div>' + '<div class="col-1">' + cart.items[index].name + "</div>" + " " + '<div class="col-2"><form><input class="box cart-qty-box" value="' + cart.items[index].quantity + '"></input></form><button class="change" itemid="'+cart.items[index].id+'" href="#">Change Quantity</button></div>' + '<div col3id="'+cart.items[index].id+'" class="col-3">$' + cart.items[index].totalPrice + "</div></section>";
+			//Adding itemid attribute to button assigns a separate id to the change button and then pass it to changeQuantity function
 
 	changeQty(index, id);
+	applyPromo();
+	totals();
 }
 
 //change quantity in shopping cart
@@ -108,6 +112,9 @@ function changeQty(index, id) {
 		var cartRowParent = qtyBoxParent.parentNode;
 		var child = cartRowParent.querySelector('section');
 		
+		//targets col-3
+		var itemQty = event.target.parentNode.nextSibling;
+
 		if (qtyBox === 0) {
 			qtyBoxParent.remove(child);
 			//remove item from cart array
@@ -116,7 +123,8 @@ function changeQty(index, id) {
 		else {
 			cart.items[index].quantity = qtyBox;
 			cart.items[index].totalPrice = cart.items[index].quantity * cart.items[index].price;
-			alert("testing");
+			//updates col-3's totalPrice
+			itemQty.innerHTML = "$" + cart.items[index].totalPrice;
 		}
 		totals();
 	}
@@ -124,7 +132,7 @@ function changeQty(index, id) {
 
 //totals
 function totals() {
-	//set these to 0 to so they don't store the initial values from the click ie: 2+1 = 3, not 2+2+1 = 4
+	//set these to 0 to so they don't store the initial values from the click ie: 2+1 = 3 doesn't become 2+2+1 = 4
 	cart.count = 0;
 	cart.total = 0;
 
@@ -138,12 +146,96 @@ function totals() {
 	cartOnPage.innerHTML = cart.count;
 }
 
+function applyPromo() {
+	var promo = document.getElementById('promo');
+
+	if (promo.value === "TANKS16") {
+  		//push promo to inUse
+  		cart.inUse.push(cart.promo[0])
+  		//apply discount to tank
+  		for (var j = 0; j < cart.items.length; j++) {
+  			if (cart.inUse[0].name === "5PERCENT") {
+    			shoppingCartTotalQty.innerHTML = "$" + parseFloat(cart.total).toFixed(2);
+    			cart.inUse.pop();
+    		}
+    		else if (cart.items[j].name === "Tank") {
+      			shoppingCartTotalQty.innerHTML = "$" + parseFloat(cart.total - (cart.items[j].totalPrice * cart.promo[0].percent)).toFixed(2);
+    		}
+  		}
+	}	
+	else if (promo.value === "ACCESS16") {
+		//it isn't pushing this promo code to the array unless it is the first code being used
+  		cart.inUse.push(cart.promo[1]);
+  		
+  		for (var e = 0; e < cart.items.length; e++) {
+  			//sets the cart total when ACCESS16 promo is applied
+  			cart.items[e].totalPrice = cart.items[e].totalPrice - (cart.items[e].totalPrice * cart.promo[1].percent);
+  			totals();
+  			//have to move this variable down here in order to get the new total, not current total from 5PERCENT promo
+  			scarvesTotal = cart.total;
+  			alert("wrong");
+  		
+  			//this code is not being run at all
+  			if (cart.inUse[0].name === "5PERCENT") {
+                if(cart.items[e].name === "Scarf") {
+                	cart.inUse.push(cart.promo[1]);
+                	//it is not checking scarvesTotal against cart.total
+  					if (scarvesTotal < cart.total) {
+  						alert("correct promo");
+    					cart.inUse.shift(); //remove the first code which is 5 % code
+    					cart.total = cart.total + (cart.total * cart.promo[2].percent); //add back the amount from 5%
+    					cart.items[e].totalPrice = cart.items[e].totalPrice - (cart.items[e].totalPrice * cart.promo[1].percent); 
+    					totals();
+      					shoppingCartTotalQty.innerHTML = "$" + parseFloat(cart.total).toFixed(2);
+      				}else{
+      					shoppingCartTotalQty.innerHTML = "$" + parseFloat(cart.total).toFixed(2);
+  				    	cart.inUse.pop(); //removes ACCESS16 code 
+      				}
+      			}
+    		}
+    		else if (cart.items[e].name === "Scarf") {
+    			cart.items[e].totalPrice = cart.items[e].totalPrice - (cart.items[e].totalPrice * cart.promo[1].percent); 
+    			totals();
+      			shoppingCartTotalQty.innerHTML = "$" + parseFloat(cart.total).toFixed(2);
+    		}
+  		}
+	}
+	else if (promo.value === "5PERCENT") {
+		if(cart.inUse.length == 0){
+  		cart.inUse.push(cart.promo[2]);
+  		//apply discount to total
+  		cart.total = cart.total - (cart.total * cart.promo[2].percent);
+  		shoppingCartTotalQty.innerHTML = "$" + parseFloat(cart.total).toFixed(2);
+  	    }
+  	    else{
+  	    	alert("Sorry only one promo code allowed.");
+  	    }
+	}
+}
+
 
 cart = {
 	"items": [
 		
 	],
 	"count": 0,
-	"total": 0
+	"total": 0,
+	"promo": [
+    	{
+      		"name": "TANKS16",
+      		"percent": .1,
+    	},
+    	{
+      		"name": "ACCESS16",
+      		"percent": .15,
+    	},
+    	{
+      		"name": "5PERCENT",
+      		"percent": .05,
+    	}
+  	],
+  	"inUse": [
+
+  	]
 };
 
